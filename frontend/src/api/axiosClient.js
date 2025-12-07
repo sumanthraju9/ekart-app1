f5a27a886495b2ba279209febc4230b5ed2456be
sumanth raju <sumanthraju4449@gmail.com>
2025-12-07 16:08:08 +0530

    Point frontend to Render backend

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://ekart-app1.onrender.com/api"
+
 });
 
 axiosClient.interceptors.request.use((config) => {
